0ac4bbf1246ce2d5a20e546c0c9e7dc470676829
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2023-05-25 23:02:06 +0300

    first commit

---
+++ src/test/java/com/example/devsecops/DevSecOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSecOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
